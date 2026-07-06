2fd2a0b8c76725321223c0bd2e543fc434428b87
Nishan Duwal <nishan.duwal@pdc1.ycotek.com>
2026-07-06 10:29:20 +0545

    BAckend setup

---
+++ backend/app.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from services.router import router
+
+app = FastAPI(
+    title="Customer Journey AI",
+    version="1.0"
+)
+
+# Allow React frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Customer Journey AI Backend Running"
+    }
